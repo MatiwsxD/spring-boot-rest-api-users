4a74e44569d43d06c918e99c0e1dfb404d8dae64
Mateo Mejia <mateomejia401@gmail.com>
2022-08-31 18:30:10 -0500

    Primer commit

---
+++ src/main/java/edu/escuelaing/IETI/Lab02Application.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.IETI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab02Application.class, args);
+	}
+
+}
